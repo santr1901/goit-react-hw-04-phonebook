9d19d6dcb0db50fcbfdd1599b0af1f8fdcd4e336
Dmitrij Sanator <32552584+santr1901@users.noreply.github.com>
2022-08-17 13:01:55 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,13 +8,9 @@ import Filter from '../Filter/Filter';
 import ContactList from '../ContactList/ContactList';
 
 const App = () => {
-  const phonebook = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-  const [contacts, setContancts] = useState(phonebook);
+  const [contacts, setContancts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts'));
+  });
   const [filter, setFilter] = useState('');
 
   const formSubmitData = (name, number) => {
@@ -45,11 +41,6 @@ const App = () => {
       contact.name.toLocaleLowerCase().includes(normalizeFilter)
     );
   };
-
-  useEffect(() => {
-    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
-    setContancts(parsedContacts);
-  }, []);
 
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
